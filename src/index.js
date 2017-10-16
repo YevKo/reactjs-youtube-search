34c8091306b5c691d7cd11bd2316e0615028db59
Yevgeniya Kobrina <yevgeniya@exove.fi>
2017-10-16 16:48:43 +0300

    delay in the refreshing search results

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by yevko on 16/10/2017.
  */
-
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -34,6 +34,8 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce((term)=>{ this.videoSearch(term) }, 300);
+
         return (
             <div>
                 <SearchBar onSearchTermChange={term=>this.videoSearch(term)}/>
