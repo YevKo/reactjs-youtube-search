441b3234b9228366fbd0e42335f77b0300091a4a
Yevgeniya Kobrina <yevgeniya@exove.fi>
2017-10-16 15:42:08 +0300

    Setting up a project structure and starting on the search form

---
+++ src/components/video_detail.js
@@ -0,0 +1,3 @@
+/**
+ * Created by yevko on 16/10/2017.
+ */
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+/**
+ * Created by yevko on 16/10/2017.
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCqc0u98SF5dhSABv0QrsuM9CvwqnivieQ';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
