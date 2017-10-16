067d0c198ece06ee5c4a52384d9b827815d77e6a
Yevgeniya Kobrina <yevgeniya@exove.fi>
2017-10-16 16:23:04 +0300

    making api call results visible

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCqc0u98SF5dhSABv0QrsuM9CvwqnivieQ';
 
@@ -28,6 +28,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
