0dc45d06616ba3d654ce6cf83afc2cb4b7010e25
Yevgeniya Kobrina <yevgeniya@exove.fi>
2017-10-16 15:51:34 +0300

    initial api call for a search bar

--- src/index.js
+++ src/index.js
@@ -2,19 +2,33 @@
  * Created by yevko on 16/10/2017.
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCqc0u98SF5dhSABv0QrsuM9CvwqnivieQ';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        // Youtube API call
+        YTSearch({key: API_KEY, term: 'aurora'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
